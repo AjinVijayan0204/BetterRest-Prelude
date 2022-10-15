45416601d9a528dcf9723636ed864396255b225b
AjinVijayan0204 <ajinvijayan0204@gmail.com>
2022-10-15 19:51:51 +0530

    Initial Commit

---
+++ BetterRestPrelude/BetterRestPreludeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestPreludeApp.swift
+//  BetterRestPrelude
+//
+//  Created by Ajin on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestPreludeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
